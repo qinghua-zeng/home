1de1a200bab05a8e2a540fdc75cd3156f965ceb1
Q Zeng <10905644+qinghua-zeng@users.noreply.github.com>
2021-09-14 11:15:24 +1200

    1

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
 
     let showAllPattern = false;
     let showAllAccessory = false;
+    let showAllWidth = false;
 
-
+    //showAllPattern
     if (showAllPattern == false) {
         $("#p05").hide();
         $("#p06").hide();
@@ -18,6 +19,7 @@ $(document).ready(function() {
         $("#p09").show();
     }
 
+    //showAllAccessory
     if (showAllAccessory == false) {
         $("#a01").hide();
         $("#a02").hide();
@@ -29,8 +31,7 @@ $(document).ready(function() {
     }
 
 
-
-
+    //=========================================
     $("#patternMore").click(function() {
         if (showAllPattern == false) {
             $("#p05").show();
@@ -51,6 +52,7 @@ $(document).ready(function() {
         }
     });
 
+    //=========================================
     $("#accessoryMore").click(function() {
         if (showAllPattern == false) {
             $("#a01").show();
@@ -62,6 +64,45 @@ $(document).ready(function() {
             $("#a02").hide();
             showAllPattern = false;
             $("#accessoryMoreText").text("All");
+        }
+    });
+
+
+    //showAllWidth
+    if (showAllWidth == false) {
+        $("#w8090").hide();
+        $("#w90100").hide();
+        $("#w100110").hide();
+        $("#w110120").hide();
+        $("#w120130").hide();
+
+
+    } else {
+        $("#w8090").show();
+        $("#w90100").show();
+        $("#w100110").show();
+        $("#w110120").show();
+        $("#w120130").show();
+    }
+
+    //=========================================
+    $("#widthMore").click(function() {
+        if (showAllWidth == false) {
+            $("#w8090").show();
+            $("#w90100").show();
+            $("#w100110").show();
+            $("#w110120").show();
+            $("#w120130").show();
+            showAllWidth = true;
+            $("#widthMoreText").text("Less");
+        } else {
+            $("#w8090").hide();
+            $("#w90100").hide();
+            $("#w100110").hide();
+            $("#w110120").hide();
+            $("#w120130").hide();
+            showAllWidth = false;
+            $("#widthMoreText").text("All");
         }
     });
 
